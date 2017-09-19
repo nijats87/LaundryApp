78d8385e4e4c26dc98e5333d8fe5c2996110d986
nijats87 <nijats87@gmail.com>
2017-09-19 15:31:23 +0400

    Spinners added

--- app/src/main/java/com/start/laundryapp/YeniSifarish.java
+++ app/src/main/java/com/start/laundryapp/YeniSifarish.java
@@ -1,30 +1,42 @@
 package com.start.laundryapp;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class YeniSifarish extends AppCompatActivity {
 
-    EditText yenisifarish_paltarsayi, yenisifarish_achiqlama, yenisifarish_menteqe, yenisifarish_sifarishnovu, yenisifarish_icramuddeti, yenisifarish_qeyd;
+public class YeniSifarish extends AppCompatActivity implements Spinner.OnItemSelectedListener {
+
+    EditText yenisifarish_paltarsayi, yenisifarish_achiqlama, yenisifarish_qeyd;
     Button sifarishver_btn;
     ImageView yenisifarish_addphoto_btn;
     RequestQueue requestQueue;
+    Spinner yenisifarish_menteqe_sp, yenisifarish_sifarishnovu_sp, yenisifarish_icramuddeti_sp;
     String sifarishVer_url = "http://138.201.157.254:8017/api/services/app/order/create";
+    public static final String icraNovleri_url = "http://138.201.157.254:8017/api/services/app/orderExecutionType/all";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,49 +45,60 @@ public class YeniSifarish extends AppCompatActivity {
 
         yenisifarish_paltarsayi = (EditText) findViewById(R.id.yenisifarish_paltarsayi_et);
         yenisifarish_achiqlama = (EditText) findViewById(R.id.yenisifarish_achiqlama_et);
-        yenisifarish_menteqe = (EditText) findViewById(R.id.yenisifarish_menteqe_et);
-        yenisifarish_sifarishnovu = (EditText) findViewById(R.id.yenisifarish_sifarishnovu_et);
-        yenisifarish_icramuddeti = (EditText) findViewById(R.id.yenisifarish_icramuddeti_et);
-        yenisifarish_qeyd = (EditText)findViewById(R.id.yenisifarish_qeyd_et);
+        yenisifarish_menteqe_sp = (Spinner) findViewById(R.id.yenisifarish_menteqe_sp);
+        yenisifarish_sifarishnovu_sp = (Spinner) findViewById(R.id.yenisifarish_sifarishnovu_sp);
+        yenisifarish_icramuddeti_sp = (Spinner) findViewById(R.id.yenisifarish_icramuddeti_sp);
+        yenisifarish_qeyd = (EditText) findViewById(R.id.yenisifarish_qeyd_et);
+        sifarishver_btn = (Button) findViewById(R.id.sifarishver_btn);
+        yenisifarish_addphoto_btn = (ImageView) findViewById(R.id.yenisifarish_addphoto_btn);
 
-        sifarishver_btn = (Button)findViewById(R.id.sifarishver_btn);
+        getData();
+    }
 
-        yenisifarish_addphoto_btn = (ImageView)findViewById(R.id.yenisifarish_addphoto_btn);
+
+    private void getData() {
 
         requestQueue = Volley.newRequestQueue(this);
 
-
-
-        sifarishver_btn.setOnClickListener(new View.OnClickListener() {
+        final JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(icraNovleri_url, new Response.Listener<JSONArray>() {
             @Override
-            public void onClick(View v) {
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, sifarishVer_url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
+            public void onResponse(JSONArray response) {
+                try {
+                    JSONObject jsonObject = new JSONObject("response");
+                    JSONArray jsonArray = jsonObject.getJSONArray("items");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject object = jsonArray.getJSONObject(i);
+                        String name = object.getString("name");
+
+                        Log.i("name", name);
+
 
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
 
-                    }
-                }) {
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map <String, String> params = new HashMap<>();
-                        params.put("numberOfClothes", yenisifarish_paltarsayi.getText().toString());
-                        params.put("orderTypeId", yenisifarish_sifarishnovu.getText().toString());
-                        params.put("executionTypeId", yenisifarish_icramuddeti.getText().toString());
-                        params.put("terminalPointId", yenisifarish_menteqe.getText().toString());
-                        params.put("notes", yenisifarish_qeyd.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-                        return params;
-                    }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-                };
-                requestQueue.add(stringRequest);
             }
         });
+
+        requestQueue.add(jsonArrayRequest);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
 
     }
 }
