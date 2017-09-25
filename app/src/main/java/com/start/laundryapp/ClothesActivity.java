6c4a3bc85bfa87493e25d54273b4ca83dc7c161e
nijats87 <nijats87@gmail.com>
2017-09-25 12:53:21 +0400

    added add photo action to clothes activity.

--- app/src/main/java/com/start/laundryapp/ClothesActivity.java
+++ app/src/main/java/com/start/laundryapp/ClothesActivity.java
@@ -1,7 +1,13 @@
 package com.start.laundryapp;
 
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ClothesActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class ClothesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothes);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.clothes_activity_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_photo:
+                //open camera
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
